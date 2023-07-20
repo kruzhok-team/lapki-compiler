bbf07908187d209bfb3d499a2f266e63d7848c3c
Mikhail Chekan <mikhail.chekan@polyus-nt.ru>
2023-07-20 15:49:07 +0800

    адаптация под формат nix-пакета
    Пакет можно разворачивать на сервере, но нужно будет
    предварительно запустить 'arduino-cli core install arduino:avr'

--- test/client.py
+++ test/client.py
@@ -1,15 +1,20 @@
 import sys
+import asyncio
+import json
+
 sys.path.append('.')
 sys.path.append('./compiler')
 sys.path.append('./compiler/fullgraphmlparser')
-import asyncio
-from Client import Client
 
+from Client import Client
 from compiler.config import SERVER_PORT
+
 
 async def test_sendSmth():
     client = Client()
     await client.doConnect(f'http://localhost:{SERVER_PORT}/ws')
-    await client.sendSMJson(path='examples/ExampleRequestSM5.json')
+    await client.sendSMJson(path='examples/ExampleRequestSMArduino.json')
+    response = json.loads(await client.ws.receive_json())
+    print(response)
 
 asyncio.run(test_sendSmth())
