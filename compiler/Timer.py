6063cc1af01cc0e5567c8ea8a777d5b542f4fc73
L140-beep <maxim.klypin@polyus-nt.ru>
2023-08-13 17:22:53 +0800

    Класс таймера

---
+++ compiler/Timer.py
@@ -0,0 +1,30 @@
+import asyncio
+from types import FunctionType
+
+
+class Timer:
+    """
+        Реализация таймера. Принимает на вход время таймаута, 
+        асинхронную функцию и ее аргументы.
+    """
+    def __init__(self, timeout: int | float,
+                 callback: FunctionType,
+                 args: tuple):
+        self._timeout = timeout
+        self._callback = callback
+        self._args = args
+        self._task = asyncio.create_task(self._work())
+
+    def start(self):
+        self._task = asyncio.create_task(self._work())
+
+    async def _work(self):
+        await asyncio.sleep(self._timeout)
+        await self._callback(*self._args)
+
+    def cancel(self):
+        self._task.cancel()
+
+    def restart(self):
+        self.cancel()
+        self.start()
