fc7e027dfa634f3872f072ebc6a8736a108c83a4
L140-beep <maxim.klypin@polyus-nt.ru>
2023-08-19 21:54:41 +0800

    AnalogOut

--- test/test_client.py
+++ test/test_client.py
@@ -307,3 +307,59 @@ async def test_digitalIn():
             await f.write(source["fileContent"])
 
     assert count_binary > 0 and count_source > 0
+
+
+@pytest.mark.asyncio
+async def test_blinker():
+    client = Client()
+    await client.doConnect(BASE_ADDR)
+    response = await client.sendSMJson("examples/arduino-blinker.json")
+    print(response)
+    dirname = strftime('%Y-%m-%d %H:%M:%S', gmtime())
+    build_path = "client/" + dirname + "/build/"
+    source_path = "client/" + dirname + "/source/"
+    Path(build_path).mkdir(parents=True)
+    Path(source_path).mkdir(parents=True)
+    count_binary = 0
+    count_source = 0
+    for binary in response["binary"]:
+        count_binary += 1
+        data = binary["fileContent"].encode('ascii')
+        data = base64.b64decode(binary["fileContent"])
+        async with async_open(build_path + binary["filename"], "wb") as f:
+            await f.write(data)
+
+    for source in response["source"]:
+        count_source += 1
+        async with async_open(source_path + source["filename"] + "." + source["extension"], "w") as f:
+            await f.write(source["fileContent"])
+
+    assert count_binary > 0 and count_source > 0
+
+
+@pytest.mark.asyncio
+async def test_analogOut():
+    client = Client()
+    await client.doConnect(BASE_ADDR)
+    response = await client.sendSMJson("examples/testAnalogOut.json")
+    print(response)
+    dirname = strftime('%Y-%m-%d %H:%M:%S', gmtime())
+    build_path = "client/" + dirname + "/build/"
+    source_path = "client/" + dirname + "/source/"
+    Path(build_path).mkdir(parents=True)
+    Path(source_path).mkdir(parents=True)
+    count_binary = 0
+    count_source = 0
+    for binary in response["binary"]:
+        count_binary += 1
+        data = binary["fileContent"].encode('ascii')
+        data = base64.b64decode(binary["fileContent"])
+        async with async_open(build_path + binary["filename"], "wb") as f:
+            await f.write(data)
+
+    for source in response["source"]:
+        count_source += 1
+        async with async_open(source_path + source["filename"] + "." + source["extension"], "w") as f:
+            await f.write(source["fileContent"])
+
+    assert count_binary > 0 and count_source > 0
