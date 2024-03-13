f09e5064ab314b7d25371ba30dbdec0f769c8f2b
L140-beep <kartoshka0302@mail.ru>
2024-03-13 18:43:41 +0800

    formatting config.py

--- compiler/config.py
+++ compiler/config.py
@@ -1,14 +1,15 @@
+"""Config file with information about paths, hosts and ports."""
 import os.path
 import inspect
 
 SERVER_PORT = 8081
-SERVER_HOST = "localhost"
+SERVER_HOST = 'localhost'
 MODULE_PATH = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
 
-BASE_DIRECTORY = MODULE_PATH + "/"  # "server/"
-BUILD_DIRECTORY = "/tmp/lapki-compiler/"
-LIBRARY_PATH = os.path.join(MODULE_PATH, "library/")
-# LIBRARY_BINARY_PATH = os.path.join(MODULE_PATH, "library/")
-SCHEMA_DIRECTORY = os.path.join(MODULE_PATH, "schemas/")
-LOG_PATH = "logs.log"  # Замените на нужную папку
+BASE_DIRECTORY = MODULE_PATH + '/'  # 'server/'
+BUILD_DIRECTORY = '/tmp/lapki-compiler/'
+LIBRARY_PATH = os.path.join(MODULE_PATH, 'library/')
+# LIBRARY_BINARY_PATH = os.path.join(MODULE_PATH, 'library/')
+SCHEMA_DIRECTORY = os.path.join(MODULE_PATH, 'schemas/')
+LOG_PATH = BUILD_DIRECTORY + 'logs.log'  # Замените на нужную папку
 MAX_MSG_SIZE = 1024 * 50  # Максимальный размер сообщения от клиента.
