cfbe61220003cae3c8a2c6d910e09f235aa141a5
L140-beep <kartoshka0302@mail.ru>
2024-04-09 11:17:56 +0800

    platforms
    wip: saving platform
    
    not-compile platforms support
    
    saving source files of platforms
    
    saving images
    
    get raw platform scheme
    
    get platform's sources
    
    get platform's images
    
    update platform
    
    remove platform by versions
    
    refactoring tests: add automatic removing test platform
    
    platform manager without staticmethods
    
    make platforms info immutable
    
    delete all platform
    
    fix bugs, create access controller
    
    add routes, refactoring routes and imports
    
    get list of platforms
    
    fix
    
    handling errors

--- compiler/GraphmlParser.py
+++ compiler/GraphmlParser.py
@@ -53,7 +53,7 @@ class GraphmlParser:
         for i in range(len(args)):
             # Можно сделать проверку значений и типов
             result[params[i].name] = args[i]
-        return result
+        return {}
 
     @staticmethod
     def _getParentNode(group_node: dict) -> dict:
