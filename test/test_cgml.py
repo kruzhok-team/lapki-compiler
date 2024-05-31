18c087b48da3c5fb74bacc89f93c40b6671e8fdf
L140-beep <kartoshka0302@mail.ru>
2024-05-31 17:31:01 +0800

    fix comment

--- test/test_cgml.py
+++ test/test_cgml.py
@@ -142,7 +142,7 @@ async def test_generating_code():
 @pytest.mark.asyncio
 async def test_compile_schemes(scheme_path: str):
     # TODO: Пофиксить баг с повторной загрузкой платформы при
-    # запуске всех тестов сразу.пш
+    # запуске всех тестов сразу.
     await AsyncPath(BUILD_DIRECTORY).mkdir(exist_ok=True)
     test_path = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
     await init_platform()
