51f9c86639cf3f2fd35b12e586005633ed60c7b3
L140-beep <kartoshka0302@mail.ru>
2024-06-18 20:48:22 +0700

    add argparse and argcomplete

--- compiler/main.py
+++ compiler/main.py
@@ -1,17 +1,26 @@
 """Root module."""
+import argparse
 from typing import NoReturn
 import asyncio
 
 from aiohttp import web
 from compiler.routes import setup_routes
-from compiler.config import SERVER_PORT, SERVER_HOST, PLATFORM_DIRECTORY
+from compiler.config import (
+    SERVER_PORT,
+    SERVER_HOST,
+    PLATFORM_DIRECTORY,
+    setup_args
+)
 from compiler.PlatformManager import PlatformManager
 from compiler.access_controller import AccessController
 from compiler.Logger import Logger
-
+import argcomplete
 
 async def main() -> NoReturn:
     """Config and running app."""
+    args_parser = argparse.ArgumentParser()
+    setup_args(args_parser)
+    argcomplete.autocomplete(args_parser)
     app = web.Application()
     setup_routes(app)
     runner = web.AppRunner(app)
