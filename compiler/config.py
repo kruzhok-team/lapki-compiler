9883b41cf1c802364b85a7b5553c3df79b495f04
L140-beep <kartoshka0302@mail.ru>
2024-06-18 20:48:22 +0700

    add argparse and argcomplete

--- compiler/config.py
+++ compiler/config.py
@@ -1,6 +1,8 @@
 """Config file with information about paths, hosts and ports."""
 import os.path
 import inspect
+import argparse
+import argcomplete
 
 SERVER_PORT = 8081
 SERVER_HOST = 'localhost'
@@ -13,3 +15,29 @@ LIBRARY_PATH = os.path.join(MODULE_PATH, 'library/')
 PLATFORM_DIRECTORY = os.path.join(MODULE_PATH, 'platforms/')
 LOG_PATH = 'logs.log'  # Замените на нужную папку
 MAX_MSG_SIZE = 1024 * 50  # Максимальный размер сообщения от клиента.
+
+
+def setup_args(parser: argparse.ArgumentParser):
+    """Add CLI args to parser."""
+    parser.add_argument('--server-port', help='Server port.')
+    parser.add_argument('--server-host', help='Server host.')
+    parser.add_argument(
+        '--library-path', help='Path to directory, '
+        'that contain platform sources.')
+    parser.add_argument('--platform-direcory', help='Path to directory, '
+                        'that contain platform json schemes.')
+    parser.add_argument('--log-path', help='Path to log file.')
+    parser.add_argument('--max-msg-size', help='Max websocket message size.')
+    argcomplete.autocomplete(parser)
+
+
+def configure(parser: argparse.ArgumentParser):
+    """
+    Actualize compiler configuration.
+
+    Priority:
+    1) Flags
+    2) Environment
+    3) config.py
+    """
+    ...
