289954c272c2a8940e9a7d590b36973d23dba128
L140-beep <klypin.maksimka@mail.ru>
2024-08-13 15:36:32 +0800

    fix

--- default.nix
+++ default.nix
@@ -1,5 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+{}:
+let
+  nixpkgs = fetchGit { 
+        url="https://github.com/NixOS/nixpkgs/";
+        rev="90f456026d284c22b3e3497be980b2e47d0b28ac";
+  };
+  pkgs = import nixpkgs {};
+in 
 pkgs.callPackage ./derivation.nix {
   python3Packages = pkgs.python310Packages;
 }
