7933f0f9557b870de76d5fd9f0491e149b1d2cd1
Maxim Klypin <70793854+L140-beep@users.noreply.github.com>
2024-08-13 18:40:01 +0800

    Синхронизация Nix-пакета (#51)
    * fix
    
    * review fixes

--- derivation.nix
+++ derivation.nix
@@ -3,6 +3,57 @@ with python3Packages;
 
 let
 
+pydantic-core = buildPythonPackage rec {
+  pname = "pydantic_core";
+  pyproject = true;
+  version = "2.16.3";
+  src = python.pkgs.fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-HKxon4Cjq6stPABIsp7qV1ERQFTwMqlBoy3kyFLFnK0=";
+  };
+  nativeBuildInputs = [
+    cargo
+    rustPlatform.cargoSetupHook
+    rustc
+    rustPlatform.maturinBuildHook
+  ];
+
+  buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
+
+  cargoDeps = rustPlatform.fetchCargoTarball {
+    inherit src;
+    name = "${pname}-${version}";
+    hash = "sha256-w1uC9tlMDOi1+qsE+U1OZkGxezPONofeG2H9wLFp5hk=";
+  };
+  doCheck = false;
+  propagatedBuildInputs = with pkgs; [ 
+    typing-extensions
+  ];
+  rustVersion = "latest";
+};
+
+pydantic = buildPythonPackage rec {
+  pname = "pydantic";
+  version = "2.6.3";
+  src = python.pkgs.fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-4HgFxMf1xoJuM6HUydR5UNfq80ho4mkPhZTS4wJB8R8=";
+  };
+  pyproject = true;
+  doCheck = false;
+  propagatedBuildInputs = with pkgs; [ 
+    hatch-fancy-pypi-readme
+    annotated-types
+    pydantic-core
+    typing-extensions
+  ];
+  nativeBuildInputs = [
+      poetry-core
+      hatchling
+      maturin
+  ];
+};
+
 aiologger = buildPythonPackage rec {
   pname = "aiologger";
   version = "0.7.0";
@@ -42,84 +93,34 @@ clang = buildPythonPackage rec {
   propagatedBuildInputs = with pkgs; [ ];
 }; 
 
-pydantic-core = buildPythonPackage rec {
-  pname = "pydantic_core";
-  pyproject = true;
-  version = "2.16.2";
-  src = python.pkgs.fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-C6UDhQ2LjcwYOR8Q3olq5R03/l/kPb+2o1xcXK0nGgY=";
-  };
-  nativeBuildInputs = [
-    cargo
-    rustPlatform.cargoSetupHook
-    rustc
-    rustPlatform.maturinBuildHook
-  ];
-
-  buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
-
-  cargoDeps = rustPlatform.fetchCargoTarball {
-    inherit src;
-    name = "${pname}-${version}";
-    hash = "sha256-qfkyies2NHhwWJ5ouBs0KASNcgi7WyazG5mNoMvAW1c=";
-  };
-  doCheck = false;
-  propagatedBuildInputs = with pkgs; [ 
-    typing-extensions
-  ];
-};
-
-pydantic = buildPythonPackage rec {
-  pname = "pydantic";
-  version = "2.6.1";
-  src = python.pkgs.fetchPypi {
-    inherit pname version;
-    sha256 = "4fd5c182a2488dc63e6d32737ff19937888001e2a6d86e94b3f233104a5d1fa9";
-  };
-  pyproject = true;
-  doCheck = false;
-  propagatedBuildInputs = with pkgs; [ 
-    hatch-fancy-pypi-readme
-    annotated-types
-    pydantic-core
-    typing-extensions
-  ];
-  nativeBuildInputs = [
-      poetry-core
-      hatchling
-      maturin
-  ];
-};
-
 cyberiadaml-py = buildPythonPackage rec {
   pname = "cyberiadaml_py";
-  version = "1.1";
+  version = "1.0";
   pyproject = true;
   src = python.pkgs.fetchPypi {
     inherit pname version;
-    sha256 = "2aa9eb0323ff8d22fcd603be874aa69cc90a3712ae97dcf1d72d3cfbfdc05dc3";
+    sha256 = "sha256-HTTk0WuOtVNYWniHd7gzSwsOfv0CtMYBcXTijDr+eME=";
   };
-  nativeBuildInputs = [
-      poetry-core
-  ];
   doCheck = false;
   propagatedBuildInputs = with pkgs; [
-    pydantic
     xmltodict
+    poetry
+    poetry-core
+    pydantic
   ];
 };
 
 typed-argument-parser = buildPythonPackage rec {
   pname = "typed-argument-parser";
   version = "1.10.0";
-  src = python.pkgs.fetchPypi {
+ src = python.pkgs.fetchPypi {
     inherit pname version;
     sha256 = "sha256-0ORjURVJ8pUd8paLGBuFbC4oG+GrH0RxQ33gjrQRPvI=";
   };
   doCheck = false;
-  propagatedBuildInputs = with pkgs; [];
+  propagatedBuildInputs = with pkgs; [typing-inspect docstring-parser];
 };
+
 
 aiofile = buildPythonPackage rec {
   pname = "aiofile";
@@ -129,7 +130,7 @@ aiofile = buildPythonPackage rec {
     sha256 = "sha256:a8f9dec17282b3583337c4ef2d1a67f33072ab80dd03608041ed9e71b88dc521";
   };
   doCheck = false;
-  propagatedBuildInputs = with pkgs; [];
+  propagatedBuildInputs = with pkgs; [caio];
 };
 
 aiofiles = buildPythonPackage rec {
@@ -151,7 +152,17 @@ aiohttp = buildPythonPackage rec {
     sha256 = "sha256:bf2e1a9162c1e441bf805a1fd166e249d574ca04e03b34f97e2928769e91ab5c";
   };
   doCheck = false;
-  propagatedBuildInputs = with pkgs; [];
+  propagatedBuildInputs = with pkgs; [  
+    attrs
+    multidict
+    async-timeout
+    yarl
+    frozenlist
+    aiosignal
+    aiodns
+    brotli
+    charset-normalizer
+];
 };
 
 argcomplete = buildPythonPackage rec {
@@ -182,7 +193,6 @@ buildPythonApplication {
   nativeBuildInputs = with pkgs; [
     poetry-core
   ];
-  
   propagatedBuildInputs = [
     aiofile
     aiohttp
@@ -206,4 +216,3 @@ buildPythonApplication {
 
   src = ./.;
 }
-
