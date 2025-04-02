65d436db319f07e8f5bf87a1be8a6ab84e8303e2
CodeR-na-r1 <sizyh06@gmail.com>
2025-04-02 18:37:14 +0800

    separate rgbLed

---
+++ compiler/library/blg-mb-1-a7/1.0/source/EyesRGBK.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "RgbLed.hpp"
+
+class EyesRGBK {
+
+    uint8_t pin;
+
+public:
+
+    bool value;
+
+    EyesRGBK() {}
+
+    // ctor
+    EyesRGBK(const uint8_t ledPin) {
+
+        if (!detail::rgbLed::isInit) {
+
+            // Инициализация модуля компонента
+            mrx::hal::pwm::enablePWMTIM2();
+
+            detail::rgbLed::isInit = true;
+        }
+        
+        pin = ledPin;
+
+        if (pin < mrx::hal::rgbLed::minPin || pin > mrx::hal::rgbLed::maxPin)
+            pin = mrx::hal::rgbLed::minPin;
+
+        mrx::hal::rgbLed::initPin(pin);
+
+        off();
+    }
+
+    void on_rgb(const uint8_t red, const uint8_t green, const uint8_t blue, const uint8_t black) {
+
+        if (!red && !green && !blue && !black)
+            return;
+
+        value = true;
+        
+        if (pin == 1) {
+
+            detail::ReservedColor1 = detail::Color{ red, green, blue, black };
+            mrx::hal::rgbLed::registerPin(pin, &detail::ReservedColor1);
+        }
+        else if (pin == 2) {
+
+            detail::ReservedColor2 = detail::Color{ red, green, blue, black };
+            mrx::hal::rgbLed::registerPin(pin, &detail::ReservedColor2);
+        }
+    }
+
+    void off() {
+
+        value = false;
+
+        mrx::hal::rgbLed::unregisterPin(pin);
+    }
+};
