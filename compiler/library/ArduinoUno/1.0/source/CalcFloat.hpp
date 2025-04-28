8b0af330407286f0260797ce88c838ae7c5e05e2
CodeR-na-r1 <sizyh06@gmail.com>
2025-04-28 16:52:46 +0800

    fix calc's

--- compiler/library/ArduinoUno/1.0/source/CalcFloat.hpp
+++ compiler/library/ArduinoUno/1.0/source/CalcFloat.hpp
@@ -5,14 +5,12 @@
 using AccTypeFloating = float;
 using ArgTypeFloating = float;
 
-class CalcFloat {
+struct CalcFloat {
 
-    bool overflowF {false};
-    bool zeroDivideF {false};
+    bool overflowF;
+    bool zeroDivideF;
 
-public:
-
-    AccTypeFloating value{};
+    AccTypeFloating value;
 
     void set(const ArgTypeFloating value) {
 
@@ -21,7 +19,7 @@ public:
 
     void add(const ArgTypeFloating value) {
 
-        const auto&& eval = detail::helpers::floating::addWithCheckOverflow(this->value, value);
+        const auto&& eval = addWithCheckOverflow(this->value, value);
 
         overflowF = eval.isError;
 
@@ -30,7 +28,7 @@ public:
 
     void sub(const ArgTypeFloating value) {
 
-        const auto&& eval = detail::helpers::floating::subWithCheckOverflow(this->value, value);
+        const auto&& eval = subWithCheckOverflow(this->value, value);
 
         overflowF = eval.isError;
 
@@ -39,7 +37,7 @@ public:
 
     void mul(const ArgTypeFloating value) {
 
-        const auto&& eval = detail::helpers::floating::mulWithCheckOverflow(this->value, value);
+        const auto&& eval = mulWithCheckOverflow(this->value, value);
 
         overflowF = eval.isError;
 
@@ -48,20 +46,20 @@ public:
 
     void div(const ArgTypeFloating value) {
 
-        const auto&& eval = detail::helpers::floating::divWithCheckOverflow(this->value, value);
+        const auto&& eval = divWithCheckOverflow(this->value, value);
 
         overflowF = eval.isError;
-        zeroDivideF = detail::helpers::floating::equalFloating(value, .0);
+        zeroDivideF = equalFloating(value, .0);
 
         this->value = eval.eval;
     }
 
     void mod(const ArgTypeFloating value) {
 
-        const auto&& eval = detail::helpers::floating::modWithCheckOverflow(this->value, value);
+        const auto&& eval = modWithCheckOverflow(this->value, value);
 
         overflowF = eval.isError;
-        zeroDivideF = detail::helpers::floating::equalFloating(value, .0);
+        zeroDivideF = equalFloating(value, .0);
 
         this->value = eval.eval;
     }
@@ -71,7 +69,8 @@ public:
         this->value = -this->value;
     }
 
-    void abs() {
+    // C has macros abs (name conflict)
+    void ABS() {
 
         if (this->value < 0)
             neg();
@@ -79,7 +78,7 @@ public:
 
     void pow(const ArgTypeFloating power) {
 
-        const auto&& eval = detail::helpers::floating::powWithCheckOverflow(this->value, power);
+        const auto&& eval = powWithCheckOverflow(this->value, power);
 
         overflowF = eval.isError;
 
@@ -88,7 +87,7 @@ public:
 
     void sqrt() {
 
-        const auto&& eval = detail::helpers::floating::sqrtWithCheckOverflow(this->value);
+        const auto&& eval = sqrtWithCheckOverflow(this->value);
 
         overflowF = eval.isError;
 
@@ -97,7 +96,7 @@ public:
 
     void log(const ArgTypeFloating base) {
 
-        const auto&& eval = detail::helpers::floating::logWithCheckOverflow(this->value, base);
+        const auto&& eval = logWithCheckOverflow(this->value, base);
 
         overflowF = eval.isError;
 
--- compiler/library/ArduinoUno/1.0/source/OverflowHelpers.hpp
+++ compiler/library/ArduinoUno/1.0/source/OverflowHelpers.hpp
@@ -1,163 +1,142 @@
 #pragma once
 
-#include <limits>
-#include <numeric>
-#include <cmath>
-#include <cstdint>
+#include <stdio.h>
+#include <math.h>
 
-namespace detail {
+int32_t MAXINT32 = 2147483646;
+int32_t MININT32 = -2147483646;
 
-    namespace helpers {
+// a - first operand
+// b - second operand
 
-        namespace integer {
+bool isAddOverflow(const int32_t a, const int32_t b) {
 
-            // a - first operand
-            // b - second operand
+    if (b > 0 && a > MAXINT32 - b)
+        return true;    // overflow
+    
+    if (b < 0 && a < MININT32 - b)
+        return true;    // underflow
 
-            using ArgType = int32_t;
-
-            bool isAddOverflow(const ArgType a, const ArgType b) {
-
-                if (b > 0 && a > std::numeric_limits<ArgType>::max() - b)
-                    return true;    // overflow
-                
-                if (b < 0 && a < std::numeric_limits<ArgType>::min() - b)
-                    return true;    // underflow
-
-                return false;
-            }
-
-            bool isSubOverflow(const ArgType a, const ArgType b) {
-
-                if (b < 0 && a > std::numeric_limits<ArgType>::max() + b)
-                    return true;    // overflow
-                
-                if (b > 0 && a < std::numeric_limits<ArgType>::min() + b)
-                    return true;    // underflow
-
-                return false;
-            }
-
-            bool isMulOverflow(const ArgType a, const ArgType b) {
-
-                if (a == -1 && b == std::numeric_limits<ArgType>::min())
-                    return true;    // overflow
-
-                if (a == std::numeric_limits<ArgType>::min() && b == -1)
-                    return true;    // overflow
-
-                if (b > 0 && a > std::numeric_limits<ArgType>::max() / b)
-                    return true;    // overflow
-                
-                if (b < 0 && a > std::numeric_limits<ArgType>::min() / b)
-                    return true;    // underflow
-
-                return false;
-            }
-
-            bool isDivOverflow(const ArgType a, const ArgType b) {
-
-                if (a == std::numeric_limits<ArgType>::min() && b == -1)
-                    return true;    // overflow
-
-                return false;
-            }
-        }
-
-        namespace floating {
-
-            using ArgType = float;
-
-            #define EPSILON 0.00001
-
-            struct pair {
-
-                bool isError{};
-                ArgType eval{};
-            };
-
-            bool equalFloating(const ArgType a, const ArgType b) {
-                
-                return fabs(a - b) < EPSILON;
-            }
-
-            // true if value is valid
-            bool isValid(const ArgType value) {
-
-                return !std::isinf(value) && !std::isnan(value) && std::numeric_limits<ArgType>::is_iec559;
-            }
-
-            // a - first operand
-            // b - second operand
-
-            pair addWithCheckOverflow(const ArgType a, const ArgType b) {
-
-                const auto&& eval = a + b;
-
-                return { !isValid(eval), eval };
-            }
-
-            pair subWithCheckOverflow(const ArgType a, const ArgType b) {
-
-                const auto&& eval = a - b;
-
-                return { !isValid(eval), eval };
-            }
-
-            pair mulWithCheckOverflow(const ArgType a, const ArgType b) {
-
-                const auto&& eval = a * b;
-
-                return { !isValid(eval), eval };
-            }
-
-            pair divWithCheckOverflow(const ArgType a, const ArgType b) {
-
-                if (equalFloating(b, .0))
-                    return { false, std::numeric_limits<ArgType>::infinity() };
-
-                const auto&& eval = a / b;
-
-                return { !isValid(eval), eval };
-            }
-
-            pair modWithCheckOverflow(const ArgType a, const ArgType b) {
-
-                if (equalFloating(b, .0))
-                    return { false, std::numeric_limits<ArgType>::infinity() };
-
-                const auto&& eval = std::fmod(a, b);
-
-                return { !isValid(eval), eval };
-            }            
-
-            pair powWithCheckOverflow(const ArgType a, const ArgType b) {
-
-                const auto&& eval = std::pow(a, b);
-
-                return { !isValid(eval), eval };
-            }
-
-            pair sqrtWithCheckOverflow(const ArgType a) {
-
-                const auto&& eval = std::sqrt(a);
-
-                return { !isValid(eval), eval };
-            }
-
-            pair logWithCheckOverflow(const ArgType a, const ArgType b) {
-
-                // b -> base of log
-                const auto&& eval = std::log(a) / std::log(b);
-
-                return { !isValid(eval), eval };
-            }
-        }
-    }
+    return false;
 }
 
-// https://stackoverflow.com/questions/15655070/how-to-detect-double-precision-floating-point-overflow-and-underflow
-// https://stackoverflow.com/questions/570669/checking-if-a-double-or-float-is-nan-in-c
-// https://stackoverflow.com/questions/42926763/the-behaviour-of-floating-point-division-by-zero
+bool isSubOverflow(const int32_t a, const int32_t b) {
 
-// log -> https://ru.stackoverflow.com/questions/1543830/%D0%9F%D0%BE%D1%87%D0%B5%D0%BC%D1%83-%D1%83-stdlog-%D0%B2-%D1%81-%D1%82%D0%BE%D0%BB%D1%8C%D0%BA%D0%BE-%D0%BE%D0%B4%D0%B8%D0%BD-%D0%B0%D1%80%D0%B3%D1%83%D0%BC%D0%B5%D0%BD%D1%82
+    if (b < 0 && a > MAXINT32 + b)
+        return true;    // overflow
+    
+    if (b > 0 && a < MININT32 + b)
+        return true;    // underflow
+
+    return false;
+}
+
+bool isMulOverflow(const int32_t a, const int32_t b) {
+
+    if (a == -1 && b == MININT32)
+        return true;    // overflow
+
+    if (a == MININT32 && b == -1)
+        return true;    // overflow
+
+    if (b > 0 && a > MAXINT32 / b)
+        return true;    // overflow
+    
+    if (b < 0 && a > MININT32 / b)
+        return true;    // underflow
+
+    return false;
+}
+
+bool isDivOverflow(const int32_t a, const int32_t b) {
+
+    if (a == MININT32 && b == -1)
+        return true;    // overflow
+
+    return false;
+}
+
+#define EPSILON 0.00001
+
+struct pair {
+
+    bool isError;
+    double eval;
+};
+
+bool equalFloating(const double a, const double b) {
+    
+    return fabs(a - b) < EPSILON;
+}
+
+// true if value is valid
+bool isValid(const double value) {
+
+    return !isinf(value) && !isnan(value);
+}
+
+// a - first operand
+// b - second operand
+
+pair addWithCheckOverflow(const double a, const double b) {
+
+    const auto&& eval = a + b;
+
+    return pair { !isValid(eval), eval };
+}
+
+pair subWithCheckOverflow(const double a, const double b) {
+
+    const auto&& eval = a - b;
+
+    return pair { !isValid(eval), eval };
+}
+
+pair mulWithCheckOverflow(const double a, const double b) {
+
+    const auto&& eval = a * b;
+
+    return pair { !isValid(eval), eval };
+}
+
+pair divWithCheckOverflow(const double a, const double b) {
+
+    if (equalFloating(b, .0))
+        return pair { false, INFINITY };
+
+    const auto&& eval = a / b;
+
+    return pair { !isValid(eval), eval };
+}
+
+pair modWithCheckOverflow(const double a, const double b) {
+
+    if (equalFloating(b, .0))
+        return pair { false, INFINITY };
+
+    const auto&& eval = fmod(a, b);
+
+    return pair { !isValid(eval), eval };
+}            
+
+pair powWithCheckOverflow(const double a, const double b) {
+
+    const auto&& eval = pow(a, b);
+
+    return pair { !isValid(eval), eval };
+}
+
+pair sqrtWithCheckOverflow(const double a) {
+
+    const auto&& eval = sqrt(a);
+
+    return pair { !isValid(eval), eval };
+}
+
+pair logWithCheckOverflow(const double a, const double b) {
+
+    // b -> base of log
+    const auto&& eval = log(a) / log(b);
+
+    return pair { !isValid(eval), eval };
+}
