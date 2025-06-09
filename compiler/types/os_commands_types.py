96c40431ae9f64b7f4742c894f09bb22ee649445
L140-beep <kartoshka0302@mail.ru>
2025-06-09 17:38:14 +0700

    review changes

--- compiler/types/os_commands_types.py
+++ compiler/types/os_commands_types.py
@@ -1,6 +1,10 @@
 from typing import Callable, Set, Awaitable, Optional
 
 
+class OSCommandsException(Exception):
+    ...
+
+
 class OSCommands:
     """Operating system-dependent commands."""
 
@@ -11,14 +15,19 @@ class OSCommands:
         self._copy = copy
         self._decode = decode
 
-    async def copy(self, path_to_libs: Set[str], target_directory: str, cwd: str) -> None:
+    async def copy(self,
+                   path_to_libs: Set[str],
+                   target_directory: str,
+                   cwd: str) -> None:
         """Copy files from path_to_libs to target directory."""
         if self._copy is None:
-            raise Exception('Using OS copy command before initialization!')
+            raise OSCommandsException(
+                'Using OS copy command before initialization!')
         return await self._copy(path_to_libs, target_directory, cwd)
 
     def decode(self, data: bytes) -> str:
         """Decode byte-data as string."""
         if self._decode is None:
-            raise Exception('Using OS copy command before initialization!')
+            raise OSCommandsException(
+                'Using OS decode command before initialization!')
         return self._decode(data)
