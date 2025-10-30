02a505f5f69e3c555b9776ca61d2fdea0e966420
glchek <gl.chekmarev@gmail.com>
2025-10-31 02:21:49 +0500

    add Buzzer to platform config, refactor Buzzer

--- compiler/library/IRPCB/1.0/source/Lapki/Buzzer.h
+++ compiler/library/IRPCB/1.0/source/Lapki/Buzzer.h
@@ -12,18 +12,17 @@ namespace Buzzer {
 
 // TODO Можно было бы добавить изменение диапазона, но оставшийся код написан на
 // макросах и его не получиться поменять в рантайме
-constexpr uint32_t top_val = BEEPER_TOP;
+constexpr uint8_t top_val = BEEPER_TOP;
 
-// 0 <= Volume <= BEEPER_TOP == 22
-void set(uint32_t Freq_Hz, uint8_t Volume) {
-    if (Volume < 0) Volume = 0;
+// 0 <= Volume <= BEEPER_TOP = 22
+void beepSpecific(uint32_t Freq_Hz, uint8_t Volume) {
     if (Volume > top_val) Volume = top_val;
     beeper.Beep(Freq_Hz, Volume);
 }
 
 void beep() { beeper.StartOrRestart(bsqBeep); }
 
-// излишне, когда функция beep занимает довольно мало времени
+// излишне: функция beep занимает довольно мало времени
 // bool isAvailable() { return beeper.IsIdle(); }
 
 };  // namespace Buzzer
