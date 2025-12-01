a4963cdbcdf67b9306ce37177e0c03ce4d3905e8
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2025-12-01 22:04:49 +0800

    Уши-Байты: расчистка

--- compiler/library/blg-mb-1-a12/1.0/source/acoustic_modem.c
+++ compiler/library/blg-mb-1-a12/1.0/source/acoustic_modem.c
@@ -1,5 +1,6 @@
 #include "commonEars.hpp"
 #include "leds.c"
+
 extern "C" {
     //
     // Потоковый декодер для передаваемых звуком данных
@@ -8,35 +9,30 @@ extern "C" {
 
     int BIT_INTERVAL = 595;
     int BIT_WINDOW = 250;
-    
+
     // Полосовой фильтр, IIR, ~2.9 КГц
     float filter_2_9_next(float x) {
-        // initMxLed(mx_c1);
-        // initMxLed(mx_c2);
-        // initMxLed(mx_c3);
-        // setMxLed(mx_c1, 1);
         static float y_past[3] = {0.0f};
         static float x_past[3] = {0.0f};
-        
+
         // Шкалу сдвинуть
         x -= 2048.0f;
-        
+
         const float feedforw[] = {1.0/40.0, 1.0/80.0, -1.0/27.0};
         const float feedback[] = {1.0, -9.0/5.0, 11.0/12.0};
-        
-        // setMxLed(mx_c1, 1);
+
         x_past[2] = x_past[1];
         x_past[1] = x_past[0];
         x_past[0] = x;
-        // setMxLed(mx_c2, 1);
+
         y_past[2] = y_past[1];
         y_past[1] = y_past[0];
-        y_past[0] = 	feedforw[0] * x_past[0] +
-        feedforw[1] * x_past[1] +
-        feedforw[2] * x_past[2] -
-        feedback[1] * y_past[1] -
-        feedback[2] * y_past[2];
-        // setMxLed(mx_c3, 1);
+        y_past[0] = feedforw[0] * x_past[0] +
+                    feedforw[1] * x_past[1] +
+                    feedforw[2] * x_past[2] -
+                    feedback[1] * y_past[1] -
+                    feedback[2] * y_past[2];
+
         return y_past[0];
     }
 
@@ -44,25 +40,25 @@ extern "C" {
     float filter_6_0_next(float x) {
         static float y_past[3] = {0.0f};
         static float x_past[3] = {0.0f};
-        
+
         // Шкалу сдвинуть
         x -= 2048.0f;
-        
+
         const float feedforw[] = {1.0/33.0, 1.0/412.0, -1.0/31.0};
         const float feedback[] = {1.0, -11.0/8.0, 14.0/15.0};
-        
+
         x_past[2] = x_past[1];
         x_past[1] = x_past[0];
         x_past[0] = x;
-        
+
         y_past[2] = y_past[1];
         y_past[1] = y_past[0];
-        y_past[0] = 	feedforw[0] * x_past[0] +
-        feedforw[1] * x_past[1] +
-        feedforw[2] * x_past[2] -
-        feedback[1] * y_past[1] -
-        feedback[2] * y_past[2];
-        
+        y_past[0] = feedforw[0] * x_past[0] +
+                    feedforw[1] * x_past[1] +
+                    feedforw[2] * x_past[2] -
+                    feedback[1] * y_past[1] -
+                    feedback[2] * y_past[2];
+
         return y_past[0];
     }
 
@@ -79,21 +75,21 @@ extern "C" {
     static struct {
         rx_func_t fn;
         uint32_t tick;
-        
+
         int preamble_ttl;
         int preamble_loc;
         float preamble_max;
-        
+
         int training_ttl;
         float training_max;
-        
+
         float bit_thresh;
         int bit_holdoff;
         int bit_pend;
         int bit_ttl;
         int bit_acc;
         float bit_max;
-        
+
         int have_byte;
         // TODO: чувствительность как параметр
     } rx = { .fn = preamble_detect, 0 };
@@ -117,12 +113,12 @@ extern "C" {
             }
             return;
         }
-        
+
         if (rx.preamble_loc >= 0) {
             rx.preamble_loc--;
             return;
         }
-        
+
         rx.training_ttl = BIT_WINDOW;
         rx.training_max = 0.0f;
         rx.fn = link_training;
@@ -131,13 +127,13 @@ extern "C" {
     static void link_training(float u, float v) {
         if (rx.training_ttl >= 0) {
             rx.training_ttl--;
-            
+
             if (ABS(u) > rx.training_max) {
                 rx.training_max = ABS(u);
             }
             return;
         }
-        
+
         rx.bit_thresh = 0.75f * rx.training_max;
         rx.bit_pend = 8;
         rx.bit_ttl = BIT_WINDOW;
@@ -152,28 +148,28 @@ extern "C" {
             rx.bit_holdoff--;
             return;
         }
-        
+
         if (rx.bit_ttl >= 0) {
             rx.bit_ttl--;
-            
+
             if (ABS(u) > rx.bit_max) {
                 rx.bit_max = ABS(u);
             }
             return;
         }
-        
+
         int bit_detected = (rx.bit_max >= rx.bit_thresh);
-        
+
         rx.bit_pend--;
         rx.bit_acc += (bit_detected << rx.bit_pend);
-        
+
         if (rx.bit_pend > 0) {
             rx.bit_holdoff = BIT_INTERVAL - BIT_WINDOW;
             rx.bit_ttl = BIT_WINDOW;
             rx.bit_max = 0.0f;
             return;
         }
-        
+
         rx.fn = preamble_detect;
         rx.have_byte = 1;
     }
@@ -191,12 +187,10 @@ extern "C" {
     // u     2.9 кГц сигнал
     // v     6.0 кГц сигнал
     void rx_advance(float u, float v) {
-        // setMxLed(mx_a5, 1);
         if (rx.fn) {
             rx.fn(u, v);
         }
         rx.tick++;
-        // setMxLed(mx_a6, 1);
     }
 
     //
@@ -217,7 +211,6 @@ extern "C" {
     initFrequency
     ( void )
     {
-        // initMxLed(mx_b2);
         RCC -> APB2ENR |= RCC_APB2ENR_TIM17EN;
         TIM17 -> CR1 &= ~TIM_CR1_CEN;
         TIM17 -> CNT = 0;
@@ -225,49 +218,20 @@ extern "C" {
         TIM17 -> PSC = 10 - 1;
         TIM17 -> DIER |= TIM_DIER_UIE;
         TIM17 -> CR1 |= TIM_CR1_CEN;
-        
+
         NVIC_EnableIRQ(TIM1_TRG_COM_TIM17_IRQn);
-        NVIC_SetPriority(TIM1_TRG_COM_TIM17_IRQn,0);
-        // setMxLed(mx_b2, 1);
+        NVIC_SetPriority(TIM1_TRG_COM_TIM17_IRQn, 0);
     }
-
-    uint32_t tim17_up = 0;
-
-    // 0	USB housekeeping
-    // 1	System
-    // 2	Data
-    #define TEST_FIXTURE_DATA_EP 2
-
-    // 32 samples, 64 bytes
-    uint16_t ch1_buf[32];
 
     void
     TIM1_TRG_COM_TIM17_IRQHandler
     ( void )
     {
-        // initMxLed(mx_a3);
-        // setMxLed(mx_a3, 1);
-        // initMxLed(mx_a5);
-        // initMxLed(mx_a6);
-        // stm32g431::ears::adcArray[0];
-        // setMxLed(mx_a3, 1);
-        //  ch1_buf[tim17_up % 32] = filter_6_0_next(adcArray[0]) + 2048;
-        //  ch1_buf[tim17_up % 32] = stm32g431::ears::adcArray[0];
-        //
-        // if (tim17_up % 32 == 0)
-            // USB_Transmit(2, (char*)ch1_buf, 64);
-        
-        // initMxLed(mx_a4);
-
         rx_advance(
             filter_2_9_next(stm32g431::ears::adcArray[0]),
             filter_6_0_next(stm32g431::ears::adcArray[1])
         );
 
-        // setMxLed(mx_a3, 1);
-        
         TIM17 -> SR &= ~TIM_SR_UIF;
-        tim17_up++;
-        // setMxLed(mx_a4, 1);
     }
 }
