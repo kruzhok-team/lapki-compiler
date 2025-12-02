cec1f5163b8edfb56eecafb3d91b89cfa94b4fff
L140-beep <kartoshka0302@mail.ru>
2025-12-02 09:44:42 +0800

    добавлен сигнал выше/ниже порога

--- compiler/library/blg-mb-1-a12/1.0/source/PhotoDiode.hpp
+++ compiler/library/blg-mb-1-a12/1.0/source/PhotoDiode.hpp
@@ -13,9 +13,11 @@ namespace detail {
 class PhotoDiode {
 
     bool isEvent { false };
-
+    bool isLessEvent { false };
     bool isEventSetting { false };
     
+    bool isGreater { false };
+
     public:
     uint16_t threshold{};
     uint8_t mode = DIODE_ON;
@@ -39,12 +41,23 @@ class PhotoDiode {
 
         value = mrx::hal::photoDiode::getSense();
 
-        if (mode == DIODE_ON && isEventSetting) {
+        if (!(mode == DIODE_ON) || !isEventSetting) return; 
 
-            if (value > threshold) {
-                isEvent = true;
-            }
+        if (value > threshold) {
+            isEvent = true;
+            isGreater = true;
+            
+            return;
         }
+
+        if (isGreater) {
+            isGreater = false;
+            isLessEvent = true;
+
+            return;
+        }
+
+        
     }
 
     void on() {
@@ -55,7 +68,15 @@ class PhotoDiode {
         mode = DIODE_OFF;
     }
 
-    bool isThresholdValue() {
+    bool lessThresholdValue() {
+
+        auto copy = isLessEvent;
+        isLessEvent = false;
+
+        return copy;
+    }
+
+    bool greaterThresholdValue() {
 
         auto copy = isEvent;
         isEvent = false;
